15e83188b1ea7645603d32c248d2397010a5a68c
Erick Sebastian <150962848+ErickL-dev@users.noreply.github.com>
2024-09-21 20:43:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,47 +5,41 @@ $(document).ready(function() {
     var btn_reset = $("#reset");
     var flowerInterval; 
 
-    
+    var audio = $('audio')[0];  // Selecciona el elemento de audio
+
     envelope.click(function() {
         open();
+        audio.play();  // Reproduce el audio al hacer clic
     });
 
-    
     btn_open.click(function() {
         open();
+        audio.play();  // Reproduce el audio al hacer clic en el botón
     });
 
-  
     btn_reset.click(function() {
         close();
         clearInterval(flowerInterval);
+        audio.pause();  // Detiene el audio al resetear
+        audio.currentTime = 0;  // Reinicia el audio al principio
     });
 
-    
     function open() {
         envelope.addClass("open").removeClass("close");
         startFlowerLoop(); 
     }
 
-    
     function close() {
         envelope.addClass("close").removeClass("open");
     }
 
-    
     function startFlowerLoop() {
         flowerInterval = setInterval(function() {
             createFlower(); 
         }, 1000); 
     }
 
-    document.getElementById('envelope').addEventListener('click', function() {
-    var audio = document.querySelector('audio');
-    audio.play();
-});
-   
     function createFlower() {
-        
         var flower = $('<img src="flor.png" class="flower">');
         flower.css({
             position: 'absolute',
@@ -55,8 +49,6 @@ $(document).ready(function() {
             height: '50px',
             opacity: 0
         });
-        
-        
         $('body').append(flower);
         flower.animate({
             opacity: 1,
